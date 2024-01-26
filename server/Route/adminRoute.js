12c3fb460b51b52c1a8146432f4647eb4400408a
shashank26-cmd <shashankbud80@gmail.com>
2024-01-26 10:12:38 +0530

    added validation

--- server/Route/adminRoute.js
+++ server/Route/adminRoute.js
@@ -1,10 +1,13 @@
 import { Router } from "express"
 import { getAllContacts, getAllUsers } from "../controller/adminController.js";
+import { authMiddleware } from "../middlewares/auth-middleware.js";
+
+
 
 const router=Router();
 
 
-router.get('/users',getAllUsers);
+router.get('/users',authMiddleware,getAllUsers);
 router.get('/contact',getAllContacts)
 
 
